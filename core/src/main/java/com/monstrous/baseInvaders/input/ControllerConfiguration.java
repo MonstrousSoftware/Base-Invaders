414483f757ff31e2c72dbd806b5fb32b727bdf2e
Monstrous <49096535+monstroussoftware@users.noreply.github.com>
2023-12-30 16:07:42 +0100

    1.0.7 repair

--- core/src/main/java/com/monstrous/baseInvaders/input/ControllerConfiguration.java
+++ core/src/main/java/com/monstrous/baseInvaders/input/ControllerConfiguration.java
@@ -1,2 +1,57 @@
-package com.monstrous.baseInvaders.input;public class ControllerConfiguration {
+package com.monstrous.baseInvaders.input;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
+import com.monstrous.baseInvaders.Settings;
+
+public class ControllerConfiguration {
+    private final Preferences preferences;
+
+    public final static String[] keyBindings = {
+        "steer left", "throttle", "brake", "reverse"
+    };
+
+    public int[] axis = { 2, 5, 4, 1};
+    public int[] sign = { -1, 1, 1, 1 };
+
+    public ControllerConfiguration() {
+        preferences = Gdx.app.getPreferences(Settings.preferencesName);
+        load();
+    }
+
+    public void resetToDefault() {
+        axis[0] = 2; sign[0] = -1;
+        axis[1] = 5; sign[1] = 1;
+        axis[2] = 4; sign[2] = 1;
+        axis[3] = 1; sign[3] = 1;
+
+    }
+
+    public void save() {
+        // save settings for next time
+        preferences.putInteger("steer", axis[0]);
+        preferences.putInteger("throttle", axis[1]);
+        preferences.putInteger("brake", axis[2]);
+        preferences.putInteger("reverse", axis[3]);
+        preferences.putInteger("steerSign", sign[0]);
+        preferences.putInteger("throttleSign",sign[1]);
+        preferences.putInteger("brakeSign", sign[2]);
+        preferences.putInteger("reverseSign", sign[3]);
+
+        preferences.flush();
+    }
+
+    public void load() {
+
+        axis[0] = preferences.getInteger("steer", 2);
+        axis[1] = preferences.getInteger("throttle", 5);
+        axis[2] = preferences.getInteger("brake", 4);
+        axis[3] = preferences.getInteger("reverse", 1);
+
+        sign[0] = preferences.getInteger("steerSign", -1);
+        sign[1] = preferences.getInteger("throttleSign", 1);
+        sign[2] = preferences.getInteger("brakeSign", 1);
+        sign[3] = preferences.getInteger("reverseSign", 1);
+
+    }
 }
--- core/src/main/java/com/monstrous/baseInvaders/screens/ControllerScreen.java
+++ core/src/main/java/com/monstrous/baseInvaders/screens/ControllerScreen.java
@@ -1,5 +1,9 @@
 package com.monstrous.baseInvaders.screens;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.controllers.Controller;
+import com.badlogic.gdx.controllers.ControllerAdapter;
+import com.badlogic.gdx.controllers.Controllers;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
@@ -7,64 +11,125 @@ import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.monstrous.baseInvaders.Settings;
-import de.golfgl.gdx.controllers.ControllerMenuStage;
+import com.monstrous.baseInvaders.input.ControllerConfiguration;
 
 
-public class KeysScreen extends MenuScreen {
+public class ControllerScreen extends MenuScreen  {
 
-    private String[][] keyBindings = {
-        { "W", "accelerate" }, {"A", "left"},
-        { "S", "brake"}, {"D", "right"},
-        { "Esc", "return to menu" },
-        { "Home", "restart" },
-        { "L", "leaderboard" },
-        { "F4", "debug view" },
-        { "F9", "show fps" },
-        { "F10", "toggle camera control" },
-        { "F11", "toggle full screen" },
-    };
+    private ControllerConfiguration config;
+    private TextButton[] axisButtons;
+    private Label[] axisLabels;
+    private Label prompt;
+    private Label controllerLabel;
 
     private GameScreen gameScreen;
+    private Controller currentController;
+    private int controlIndex = -1;
+    private Adapter testAdapter;
 
+    class Adapter extends ControllerAdapter {
+        @Override
+        public boolean axisMoved(Controller controller, int axisIndex, float value) {
+            if(controlIndex < 0)
+                return false;
+            Gdx.app.log("controller", "axis moved: " + axisIndex + " : " + value);
+             if (Math.abs(value) > 0.5f) {
+                 config.sign[controlIndex] = (int) Math.signum(value);
+                 prompt.setVisible(false);
+                 config.axis[controlIndex] = axisIndex;
+                 axisLabels[controlIndex].setText("   axis: " + axisIndex + (config.sign[controlIndex]>0?"+":"-"));
+                 controlIndex = -1;
+                 Controllers.removeListener(testAdapter);
+                 Controllers.addListener(game.controllerToInputAdapter);
+            }
 
-    public KeysScreen(Main game, GameScreen gameScreen) {
+            return true;
+        }
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+        if(currentController != null)
+            controllerLabel.setText(currentController.getName());
+        else
+            controllerLabel.setText("None");
+    }
+
+    public ControllerScreen(Main game, GameScreen gameScreen) {
         super(game);
         this.gameScreen = gameScreen;
+        config = game.controllerConfiguration;
+        testAdapter = new Adapter();
+
+
+        // controller
+        if (Settings.supportControllers) {
+            currentController = Controllers.getCurrent();
+            if (currentController != null) {
+                Gdx.app.log("current controller", currentController.getName());
+            } else {
+                Gdx.app.log("current controller", "none");
+            }
+        }
     }
 
 
     @Override
-   protected void rebuild() {
-       stage.clear();
+    protected void rebuild() {
+        stage.clear();
+
+        controllerLabel = new Label("...", skin);
+        if(currentController != null)
+            controllerLabel.setText(currentController.getName());
+        else
+            controllerLabel.setText("None");
+
+        Table screenTable = new Table();
+        screenTable.setFillParent(true);
+
+       // Label title = new Label("CONTROLLER BINDINGS:", skin);
+
+        screenTable.add(controllerLabel).row();
+
+        TextButton defaultButton = new TextButton("Default Configuration", skin);
+
+        TextButton continueButton = new TextButton("Continue", skin);
+
+        axisButtons = new TextButton[4];
+        axisLabels = new Label[4];
+        prompt = new Label("Move joystick to select", skin);
+        prompt.setVisible(false);
+
+        float pad = 3f;
+        for (int i = 0; i < config.axis.length; i++) {
+            axisButtons[i] = new TextButton(config.keyBindings[i], skin);
+            axisLabels[i] = new  Label("   axis: " + config.axis[i]+(config.sign[i]>0?"+":"-"), skin);
+            screenTable.add(axisButtons[i]).width(300).center().pad(pad);
+            screenTable.add(axisLabels[i]).width(50).center().pad(pad);
+            screenTable.row();
+        }
+        screenTable.add(prompt).row();
+
+        screenTable.add(defaultButton).colspan(2).pad(20).row();
+        screenTable.add(continueButton).colspan(2).pad(20).row();
 
 
-       Table screenTable = new Table();
-       screenTable.setFillParent(true);
+        screenTable.pack();
 
-       Label title = new Label("KEYS:", skin);
-
-       TextButton continueButton = new TextButton("Continue", skin);
-
-       float pad = 3f;
-       for(int i = 0; i < keyBindings.length; i++) {
-           screenTable.add(new Label(keyBindings[i][0], skin)).width(100).center().pad(pad);
-           screenTable.add(new Label(keyBindings[i][1], skin)).pad(pad).left().row();
-       }
-
-       screenTable.add(continueButton).colspan(2).pad(20).row();
+        screenTable.setColor(1, 1, 1, 0);                   // set alpha to zero
+        screenTable.addAction(Actions.fadeIn(3f));           // fade in
 
 
-       screenTable.pack();
+        stage.addActor(screenTable);
 
-       screenTable.setColor(1,1,1,0);                   // set alpha to zero
-       screenTable.addAction(Actions.fadeIn(3f));           // fade in
-
-
-       stage.addActor(screenTable);
-
-       // set up for keyboard/controller navigation
-        if(Settings.supportControllers) {
+        // set up for keyboard/controller navigation
+        if (Settings.supportControllers) {
             stage.clearFocusableActors();
+            for (int i = 0; i < config.axis.length; i++) {
+                stage.addFocusableActor(axisButtons[i]);
+            }
+            stage.addFocusableActor(defaultButton);
             stage.addFocusableActor(continueButton);
             stage.setFocusedActor(continueButton);
             super.focusActor(stage, continueButton);    // highlight focused actor
@@ -72,18 +137,43 @@ public class KeysScreen extends MenuScreen {
             stage.setEscapeActor(continueButton);
         }
 
+        defaultButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                super.clicked(event, x, y);
+                playSelectNoise();
+                config.resetToDefault();
+                for(int i = 0; i < axisLabels.length; i++)
+                    axisLabels[i].setText("   axis: " + config.axis[i] + (config.sign[i]>0?"+":"-"));
+
+            }
+        });
+
         continueButton.addListener(new ClickListener() {
-           @Override
-           public void clicked(InputEvent event, float x, float y) {
-               super.clicked(event, x, y);
-               playSelectNoise();
-               if(gameScreen != null)
-                   game.setScreen(new PauseMenuScreen( game, gameScreen ));
-               else
-                   game.setScreen(new MainMenuScreen( game ));
-           }
-       });
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                super.clicked(event, x, y);
+                playSelectNoise();
+                if(gameScreen != null)
+                    game.setScreen(new PauseMenuScreen( game, gameScreen ));
+                else
+                    game.setScreen(new MainMenuScreen( game ));
+            }
+        });
 
-   }
-
+        for (int i = 0; i < config.axis.length; i++) {
+            int finalI = i;
+            axisButtons[i].addListener(new ClickListener() {
+                @Override
+                public void clicked(InputEvent event, float x, float y) {
+                    super.clicked(event, x, y);
+                    playSelectNoise();
+                    prompt.setVisible(true);
+                    Controllers.removeListener(game.controllerToInputAdapter);
+                    Controllers.addListener(testAdapter);
+                    controlIndex = finalI;
+                }
+            });
+        }
+    }
 }
